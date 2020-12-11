11d400b00eb09014b0c6d9e063f4fc1151f255fc
Nigless <nigless>
2020-12-11 21:31:36 +0000

    Deleted trash in app.tsx

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,17 +1,5 @@
 import * as React from 'react';
-import styled from 'styled-components';
-import Ratio from './ratio';
-
-const Div = styled.div({
-	position: 'relative',
-});
 
 export default function App() {
-	return (
-		<>
-			<Ratio x={2} y={1}>
-				<Div />
-			</Ratio>
-		</>
-	);
+	return <></>;
 }
