bcb8e4faff9a73bcf96cee3b15256930b158a54c
Nigless <nigless>
2020-12-11 21:22:42 +0000

    Added properties-interfaces.ts

---
+++ src/properties-interfaces.ts
@@ -0,0 +1,22 @@
+﻿import { ReactNode } from 'react';
+
+interface PropertiesParent {
+	children?: ReactNode;
+}
+
+interface PropertiesSlyled {
+	className?: string;
+}
+
+interface PropertiesWrapper {
+	wrap?: string | React.ComponentType<any>;
+}
+
+interface PropertiesBasic extends PropertiesParent, PropertiesSlyled {}
+
+export type {
+	PropertiesParent,
+	PropertiesSlyled,
+	PropertiesBasic,
+	PropertiesWrapper,
+};
