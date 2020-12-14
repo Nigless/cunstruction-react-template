b424b21461b3d159c9b86364c808752e649d47e9
Nigless <nigless.gaming@gmail.com>
2020-12-14 13:42:07 +0300

    Added Accordion component

---
+++ src/components/accordion/accordion-content.tsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import {
+	PropertiesDynamicCildren,
+	PropertiesSwitchable,
+} from '../../properties-interfaces';
+
+interface Properties extends PropertiesDynamicCildren<PropertiesSwitchable> {
+	header: (properties: {
+		active: boolean;
+		onClick: (event: any) => void;
+	}) => JSX.Element;
+}
+
+export default function AccordionContent(properties: Properties): JSX.Element {
+	return <></>;
+}
