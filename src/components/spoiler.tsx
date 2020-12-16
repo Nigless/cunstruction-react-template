215ecd9224b2317385a1e154ceffe0555f150806
Nigless <nigless.gaming@gmail.com>
2020-12-16 19:59:10 +0300

    Added Spoiler component

---
+++ src/components/spoiler.tsx
@@ -0,0 +1,32 @@
+﻿import React, { useState } from 'react';
+import {
+	PropertiesDynamicChildren,
+	PropertiesStyled,
+	PropertiesSwitchable,
+} from '../properties-interfaces';
+
+interface Properties
+	extends PropertiesStyled,
+		PropertiesDynamicChildren<PropertiesSwitchable> {
+	header: (properties: {
+		active: boolean;
+		onClick: (event: any) => void;
+	}) => JSX.Element;
+}
+
+export default function Spoiler(properties: Properties): JSX.Element {
+	const clickHandler = () => setState(!state);
+
+	const [state, setState] = useState(false);
+
+	const { children, className, header } = properties;
+	const Header = header;
+	const Content = children;
+
+	return (
+		<div className={className}>
+			<Header active={state} onClick={clickHandler.bind(this)} />
+			<Content active={state} />
+		</div>
+	);
+}
