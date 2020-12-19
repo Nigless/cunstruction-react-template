5d105ae624bd3ba05a55783b3168c49f5ad66833
Nigless <nigless.gaming@gmail.com>
2020-12-19 22:12:42 +0300

    Fixed merge

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './components/App/app';
 
 let mountNode = document.getElementById('app');
 ReactDOM.render(<App />, mountNode);
