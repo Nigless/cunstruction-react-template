d98665b580cece9b3cc80648e74cf34c3d784dc3
Nigless <nigless>
2020-12-20 17:24:43 +0000

    Removed useless code

--- src/components/App/link.tsx
+++ src/components/App/link.tsx
@@ -1,29 +1,7 @@
-﻿import React from 'react';
-import styled from 'styled-components';
-import { PropertiesBasic } from '../../properties-interfaces';
+﻿import styled from 'styled-components';
 
-const Root = styled.a({
+const Link = styled.a({
 	color: 'inherit',
 	textDecoration: 'none',
 });
-
-interface Properties extends PropertiesBasic {
-	href: string;
-	ariaLabel?: string;
-}
-
-export default function App(properties: Properties) {
-	const { href, children, className, ariaLabel } = properties;
-
-	return (
-		<Root
-			{...{
-				className,
-				href,
-				ariaLabel,
-			}}
-		>
-			{children}
-		</Root>
-	);
-}
+export default Link;
