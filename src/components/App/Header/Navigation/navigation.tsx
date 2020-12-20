3665f0177500ffbe195d02021f1abae59b5730b4
Nigless <nigless.gaming@gmail.com>
2020-12-20 17:04:30 +0300

    Added Contacts

--- src/components/App/Header/Navigation/navigation.tsx
+++ src/components/App/Header/Navigation/navigation.tsx
@@ -1,15 +1,25 @@
 ﻿import React from 'react';
 import styled from 'styled-components';
 import Container from '../../../container';
+import Contacts from './contacts';
 
 const Root = styled.div({
 	position: 'relative',
 });
 
+const Hr = styled.hr({
+	position: 'absolute',
+	left: 0,
+	right: 0,
+	margin: 0,
+});
+
 export default function Navigation() {
 	return (
 		<Root>
-			<Container maxWidth={1200}></Container>
+			<Container maxWidth={1200}>
+				<Contacts />
+			</Container>
 		</Root>
 	);
 }
