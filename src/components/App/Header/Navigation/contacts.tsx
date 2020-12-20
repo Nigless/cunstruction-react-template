1588aa4a1e0d29b44c52ab9aaea43995a0c76b57
Nigless <nigless>
2020-12-20 14:04:30 +0000

    Added Contacts

---
+++ src/components/App/Header/Navigation/contacts.tsx
@@ -0,0 +1,40 @@
+﻿import React from 'react';
+import styled from 'styled-components';
+import { PropertiesStyled } from '../../../../properties-interfaces';
+import Flex from '../../../flex';
+import List from '../../../list';
+
+const Root = styled(Flex)({
+	height: 50,
+	'*+*': {
+		marginLeft: 5,
+	},
+});
+
+const RightBlock = styled(Flex)({
+	flexGrow: 1,
+});
+
+const SocialList = styled(List)({
+	padding: 0,
+	margin: 0,
+	listStyle: 'none',
+});
+
+export default function Contacts({ className }: PropertiesStyled) {
+	return (
+		<Root align="center" className={className}>
+			<a href="tel:+">+(123) 1234-567-8901</a>
+			<a href="mailto:">info@domain.com</a>
+			<span>Mon - Sat 8:00 - 17:30, Sunday - CLOSED</span>
+			<RightBlock align="center" justify="flex-end">
+				<SocialList>
+					<a href="#">Twitter</a>
+					<a href="#">Facebook</a>
+					<a href="#">Linkedin</a>
+					<a href="#">Google+</a>
+				</SocialList>
+			</RightBlock>
+		</Root>
+	);
+}
