8ecee3950e48ddae0e3f16382c9aa9e240265fa5
Nigless <nigless.gaming@gmail.com>
2020-12-20 19:09:44 +0300

    Deleted useless constant

--- src/components/App/Header/Navigation/navigation.tsx
+++ src/components/App/Header/Navigation/navigation.tsx
@@ -7,13 +7,6 @@ const Root = styled.div({
 	position: 'relative',
 });
 
-const Hr = styled.hr({
-	position: 'absolute',
-	left: 0,
-	right: 0,
-	margin: 0,
-});
-
 export default function Navigation() {
 	return (
 		<Root>
