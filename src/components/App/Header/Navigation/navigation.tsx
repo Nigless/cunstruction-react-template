5cdab804f20e9c9f841492f38dbea27086ad79ee
Nigless <nigless>
2020-12-21 12:56:56 +0000

    Added icons in contacts

--- src/components/App/Header/Navigation/navigation.tsx
+++ src/components/App/Header/Navigation/navigation.tsx
@@ -1,14 +1,16 @@
 ﻿import React from 'react';
 import styled from 'styled-components';
 import Container from '../../../container';
-import Contacts from './contacts';
+import Contacts from './Contacts/contacts';
 
 const Root = styled.div({
 	position: 'relative',
 });
+
 const Body = styled(Container)({
 	padding: '0 30px',
 });
+
 export default function Navigation() {
 	return (
 		<Root>
