be9f1bf68d0bde610abfb33847f509ed87b7571f
Nigless <nigless.gaming@gmail.com>
2020-12-22 17:50:01 +0300

    Added Menu

--- src/components/App/Header/Navigation/navigation.tsx
+++ src/components/App/Header/Navigation/navigation.tsx
@@ -2,6 +2,7 @@
 import styled from 'styled-components';
 import Container from '../../../container';
 import Contacts from './Contacts/contacts';
+import Menu from './Menu/menu';
 
 const Root = styled.div({
 	position: 'relative',
@@ -11,11 +12,22 @@ const Body = styled(Container)({
 	padding: '0 30px',
 });
 
+const Hr = styled.hr({
+	position: 'absolute',
+	left: 0,
+	right: 0,
+	margin: 0,
+	border: 'solid 1px #000',
+	borderBottom: 0,
+});
+
 export default function Navigation() {
 	return (
 		<Root>
 			<Body maxWidth={1200}>
 				<Contacts />
+				<Hr />
+				<Menu />
 			</Body>
 		</Root>
 	);
