49f524534b25e3261daf8b76b9f35e12b337bf7e
Nigless <nigless>
2020-12-30 18:15:19 +0000

    Added fontFace function

---
+++ src/utils/font-face.ts
@@ -0,0 +1,7 @@
+﻿import { createGlobalStyle, css, CSSObject } from 'styled-components';
+
+export default function fontFace(...fonts: CSSObject[]) {
+	return createGlobalStyle`
+			${fonts.map((elem) => `@font-face {${css(elem)}}`).join('')}
+		`;
+}
