0d020be2c1881f43c4c94456f5a9c6af83e87dbe
Nigless <nigless>
2020-12-31 15:25:18 +0000

    Added fonts

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,14 +1,35 @@
 ﻿import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+import fontFace from '../../utils/font-face';
 import Content from './Content/content';
+import barlowFontExtraBold from './fonts/Barlow-ExtraBold.ttf';
+import barlowFontMedium from './fonts/Barlow-Medium.ttf';
+import barlowFontRegular from './fonts/Barlow-Regular.ttf';
 import Header from './Header/header';
 import navigation from './Navigation/navigation';
+
+const Fonts = fontFace(
+	{
+		fontFamily: 'Barlow',
+		src: `url(${barlowFontRegular}) format("truetype")`,
+	},
+	{
+		fontFamily: 'Barlow',
+		src: `url(${barlowFontMedium}) format("truetype")`,
+		fontWeight: 500,
+	},
+	{
+		fontFamily: 'Barlow',
+		src: `url(${barlowFontExtraBold}) format("truetype")`,
+		fontWeight: 900,
+	}
+);
 
 const GlobalStyle = createGlobalStyle({
 	body: {
 		margin: 0,
 		fontSize: 14,
-		fontFamily: 'Roboto',
+		fontFamily: 'Barlow',
 	},
 });
 
@@ -21,7 +42,9 @@ const Navigation = styled(navigation)({
 export default function App() {
 	return (
 		<>
+			<Fonts />
 			<GlobalStyle />
+
 			<Navigation />
 			<Header />
 			<Content />
