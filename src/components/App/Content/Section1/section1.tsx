3df63fbcebb2993b22e6a6338dd180cb0ede6caa
Nigless <nigless>
2021-01-02 13:57:23 +0000

    Added component Section1

---
+++ src/components/App/Content/Section1/section1.tsx
@@ -0,0 +1,31 @@
+﻿import React from 'react';
+import styled from 'styled-components';
+import Flex from '../../../flex';
+import article from './article';
+import heading from './heading';
+import image from './services1.png';
+
+const Heading = styled(heading)({
+	margin: 0,
+	marginBottom: 60,
+});
+
+const Article = styled(article)({
+	flex: 1,
+	'&+&': {
+		marginLeft: 30,
+	},
+});
+
+export default function Section1() {
+	return (
+		<section>
+			<Heading as="h2">OUR SERVICES</Heading>
+			<Flex>
+				<Article image={<img src={image} />} label="Heading" />
+				<Article image={<img src={image} />} label="Heading" />
+				<Article image={<img src={image} />} label="Heading" />
+			</Flex>
+		</section>
+	);
+}
--- src/components/App/Content/content.tsx
+++ src/components/App/Content/content.tsx
@@ -1,37 +1,17 @@
 ﻿import React from 'react';
 import styled from 'styled-components';
 import Container from '../../container';
-import Flex from '../../flex';
-import article from './article';
-import heading from './heading';
-import image from './servicess1.png';
-
-const Article = styled(article)({
-	flex: 1,
-	'&+&': {
-		marginLeft: 30,
-	},
-});
+import Section1 from './Section1/section1';
 
 const Section = styled(Container)({
 	marginTop: 183,
 });
 
-const Heading = styled(heading)({
-	margin: 0,
-	marginBottom: 60,
-});
-
 export default function Content() {
 	return (
 		<main>
-			<Section wrap="section" maxWidth={1200}>
-				<Heading as="h2">OUR SERVICES</Heading>
-				<Flex>
-					<Article image={<img src={image} />} label="Heading" />
-					<Article image={<img src={image} />} label="Heading" />
-					<Article image={<img src={image} />} label="Heading" />
-				</Flex>
+			<Section maxWidth={1200}>
+				<Section1 />
 			</Section>
 		</main>
 	);
